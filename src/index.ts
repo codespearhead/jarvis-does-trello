0e2b83de9aad2ce739f2cc263b42f66767f8a601
Paguiar735 <noreply@paguiar.dev>
2022-02-25 17:34:43 -0400

    feat: set up jest and refactor directory tree

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import * as jarvisTrello from "./config/Trello/jarvisTrelloFunctions";
+import * as jarvisTrello  from "./config/Trello/jarvisTrelloFunctions";
 
 async function main() {
-    //let a = await jarvisTrello.getList(process.env.TRELLO_LIST1, ["cards", "name", "id", "start", "due"]);
-    let a = await jarvisTrello.getCard(process.env.TRELLO_CARD1, ["name", "id"]);
-    console.log(a);
+    let response = await jarvisTrello.getCard(process.env.TRELLO_CARD1, ["name", "id"]);
+    console.log(response);
 }
 
 main()
