76b50d0591cd64a564721fd545b1006e8034a3dc
Paguiar735 <noreply@paguiar.dev>
2022-03-02 19:58:58 -0400

    feat: make functions return js objects

--- src/config/util/axiosTrelloInstance.ts
+++ src/config/util/axiosTrelloInstance.ts
@@ -1,23 +1,14 @@
 import axios from "axios";
-import { AxiosResponse, AxiosRequestConfig } from "axios"
-
+import { AxiosResponse, AxiosInstance } from "axios"
 import dotenv from "dotenv"
+
+dotenv.config({path: ".test.env"})
 dotenv.config()
 
-const axiosTrello = axios.create({
+export { AxiosResponse }
+export const axiosTrello: AxiosInstance = axios.create({
     baseURL: 'https://api.trello.com/1',
-    timeout: 10000,
-    headers: {
-        'Accept': 'application/json',
-        'key': process.env.TRELLO_API_KEY,
-        'token': process.env.TRELLO_API_TOKEN
-    }
-});
-
-const axiosPutTrello = axios.create({
-    method: 'put',
-    baseURL: 'https://api.trello.com/1',
-    timeout: 10000,
+    timeout: 12000,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
@@ -27,11 +18,3 @@ const axiosPutTrello = axios.create({
         'token': process.env.TRELLO_API_TOKEN
     }
 });
-
-export {
-    axiosTrello,
-    AxiosResponse,
-    axiosPutTrello,
-    axios,
-    AxiosRequestConfig
-};
