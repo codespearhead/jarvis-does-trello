58cf8a98918b5745dd65f290a4bc4326eb23a103
Paguiar735 <noreply@paguiar.dev>
2022-03-10 22:48:17 -0400

    fix: set up for first publication

--- src/Trello/Tests/minimalWorkingExample.ts
+++ src/Trello/Tests/minimalWorkingExample.ts
@@ -3,12 +3,12 @@ import * as jarvis from "../index"
 // NodeJS-specific. Run "npm i dotenv" first
 import dotenv from "dotenv"
 dotenv.config({path: ".test.env"})
-dotenv.config()
+dotenv.config({path: ".env"})
 
 jarvis.addDeadline({
     auth: {
-        key: process.env.TRELLO_API_KEY,
-        token: process.env.TRELLO_API_TOKEN,
+        key: process.env.TRELLO_API_KEY_BOT_7,
+        token: process.env.TRELLO_API_TOKEN_BOT_7,
     },
     dateIntervalInDays: 10,
     idList: process.env.TRELLO_LIST2
