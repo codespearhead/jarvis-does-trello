661a97c41301aa15b34ad3058605d4a97fde3dde
Paguiar735 <noreply@paguiar.dev>
2022-03-15 13:32:45 -0400

    [REFACTOR]: bump project version number

--- src/jarvis-does-trello/Util/generalFunctions/listFilesInFolder.ts
+++ src/jarvis-does-trello/Util/generalFunctions/listFilesInFolder.ts
@@ -1,11 +1,11 @@
 import fs from 'fs';
 
-export function listFilesInFolder (args: {pathTofolder: string, enconding: any}) {
+export function listFilesInFolder (args: {pathToFolder: string, enconding: any}) {
     let encoding = args["enconding"];
     if (encoding)
         encoding = "UTF-8"
     return new Promise(function(resolve, reject) {
-        fs.readdir(args["pathTofolder"],encoding, function(err, filenames){
+        fs.readdir(args["pathToFolder"],encoding, function(err, filenames){
             if (err) 
                 reject(err)
             else 
